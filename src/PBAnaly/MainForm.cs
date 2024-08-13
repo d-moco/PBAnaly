9f75b091b9262bfc3552f2e18ef570166b777ed9
耿东亚 <m_moco.geng@smartmore.com>
2024-08-13 21:54:37 +0800

    update 仓库代码框架

---
+++ src/PBAnaly/MainForm.cs
@@ -0,0 +1,122 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Windows.Forms;
+
+namespace PBAnaly
+{
+    public partial class MainForm : MaterialForm
+    {
+        MaterialSkinManager materialSkinManager;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            UIInit();
+        }
+
+        public void UIInit()
+        {
+            materialSkinManager = MaterialSkinManager.Instance; // 初始化 MaterialSkinManager 实例
+            materialSkinManager.AddFormToManage(this);  // 将要应用 Material Design 的窗体添加到管理列表中
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;   // Theme 属性用来设置整体的主题
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.Indigo700, TextShade.WHITE);    // ColorScheme 属性来设置配色方案
+        }
+
+        private void materialSwitch_UI_CheckedChanged(object sender, EventArgs e)
+        {
+            if(materialSwitch_UI.Checked == true)
+            {
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;   // Theme 属性用来设置整体的主题
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.Purple900, Primary.Purple800, Primary.BlueGrey300, Accent.Purple700, TextShade.WHITE);	// ColorScheme 属性来设置配色方案
+            }
+            else
+            {
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;   // Theme 属性用来设置整体的主题
+                materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.Indigo700, TextShade.WHITE);	// ColorScheme 属性来设置配色方案
+            }
+        }
+
+        private void materialButton_changeFormSize_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "适配窗口");
+            }
+        }
+
+        private void materialButton_imageChange_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "图像变换");
+            }
+        }
+
+        private void materialButton_fakeColor_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "伪彩");
+            }
+        }
+
+        private void materialButton_imageInfo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "图像信息");
+            }
+        }
+
+        private void materialButton_resetImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "重置原图");
+            }
+        }
+
+        private void materialButton_inverse_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "反值");
+            }
+        }
+
+        private void materialButton_save_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "Ctrl + S 保存");
+            }
+        }
+
+        private void materialButton_return_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "Ctrl + Z 撤銷");
+            }
+        }
+
+        private void materialButton_forward_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button btn = sender as Button;
+                this.btnStartUpToolTip.SetToolTip(btn, "Ctrl + Y 重做");
+            }
+        }
+    }
+}
